aa76e363d629539c0f49082b152de8bbf830b11d
Luis Sebastián Huerta <luissebastianhuerta@gmail.com>
2022-12-08 11:24:50 +0100

    feat: day 07 almost working 🤞

--- lib/device_space.ex
+++ lib/device_space.ex
@@ -8,12 +8,10 @@ defmodule AdventOfCode2022.DeviceSpace do
     |> Enum.drop(1)
     |> Enum.map(&(String.split(&1, "\n") |> Enum.filter(fn x -> x != "" end)))
     |> execute_commands([], %{})
-    |> Enum.filter(fn {_, value} -> value > dir_size end)
-    |> IO.inspect()
+    |> Enum.filter(fn {_, value} -> value < dir_size end)
     |> Enum.map(fn {key, value} -> value end)
     |> Enum.sum()
   end
-
 
   def execute_commands([], breadcrumbs, dir_sizes_map), do: dir_sizes_map
 
@@ -37,6 +35,7 @@ defmodule AdventOfCode2022.DeviceSpace do
         &(String.split(&1, " ")
           |> List.first())
       )
+      |> IO.inspect()
       |> Enum.filter(&is_file?(&1))
       |> Enum.map(&parse_int!(&1))
       |> Enum.sum()
@@ -45,25 +44,32 @@ defmodule AdventOfCode2022.DeviceSpace do
     dir_sizes_map =
       dir_sizes_map
       |> Enum.map(fn {key, value} ->
-        {key, value + total_size}
+        # si el key está en el breadcrumbs, entonces es un directorio que está en el path actual
+        # y por lo tanto hay que incrementar su tamaño
+        if Enum.member?(breadcrumbs, key) do
+          {key, value + total_size}
+        else
+          {key, value}
+        end
       end)
       |> Map.new()
+      |> IO.inspect()
 
     execute_commands(rest, breadcrumbs, dir_sizes_map)
   end
 
   # cd
   def execute_commands([["cd " <> directory | params] | rest], breadcrumbs, dir_sizes_map) do
-    dir_sizes_map =
-      dir_sizes_map
-      # vamos a suponer que no entra dos veces en el mismo directorio. sino habría que cambiar esta línea
-      |> Map.put(directory, 0)
-
     case directory do
       ".." ->
         execute_commands(rest, breadcrumbs |> Enum.drop(-1), dir_sizes_map)
 
       directory_name ->
+        dir_sizes_map =
+          dir_sizes_map
+          # vamos a suponer que no entra dos veces en el mismo directorio. sino habría que cambiar esta línea
+          |> Map.put(directory, 0)
+
         execute_commands(rest, breadcrumbs ++ [directory_name], dir_sizes_map)
     end
   end
